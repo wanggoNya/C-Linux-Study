105fff8d2737177a722c253fb4af15893fb5b221
wanggoNya <tndus6384@naver.com>
2022-08-15 10:46:29 +0900

    파일 입출력 실습 완료

--- c-training/12_파일 입출력.c
+++ c-training/12_파일 입출력.c
@@ -1,43 +1,60 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-/* 문제
-읽을 파일 입력
-해당 파일에서 찾고 싶은 문자열 입력
-대소문자 구별 없이 문자열 찾기
-모두 대문자로 변경
-기존파일명에 _capital 붙여서 파일 쓰기 */
+char *strcasestr(const char *haystack, const char *needle) {
+	int size = strlen(needle);
 
-int main(void) {
-//	FILE *fp = NULL;
-	FILE *fp;
-	char fname[256];
-	char word[256];
-	char buffer[256];
-	int line = 0;
+	while(*haystack) {
+		if(strncasecmp(haystack, needle, size) == 0) {
+			return (char *)haystack;
+		}
+		haystack++;
+	}
+	return NULL;
+}
+
+int main() {
+	FILE *fp, *fp_cap;
+	char fname[256], temp[256], fname_capital, word[256], buffer[256], *corr, *corrTemp;
 
 	printf("파일 이름을 입력하세요 : ");
 	scanf("%s", fname);
 
-	fp = fopen("a.txt", "w");
+	fp = fopen(fname, "r");
+	strcpy(temp, fname); // temp에 fname 복사
 
-	if((fp = fopen(fname, "r")) = NULL) {
-		printf(stderr. "파일 %s 을(를) 여는데 실패했습니다. \n", fname);
+	if((fp = fopen(fname, "r")) == NULL) {
+		fprintf(stderr, "파일 %s 을(를) 여는데 실패했습니다. \n", fname);
 		return 0;
 	}
 
+	char *namePlus = "_capital.txt"; // 두 번째 파일이름 설정
+	char *fnameCut = strtok(temp, "."); // temp에서 문자 . 을 기준으로 문자열 자르기
+	strcat(fnameCut, namePlus); // 문자열 붙이기
+
 	printf("파일 열기에 성공했습니다. \n대문자로 변경하고자 하는 문자열을 입력 하세요 : ") ;
 	scanf("%s", word);
+	int len = (int)strlen(word);
+	int i, j;
+
+	fp_cap = fopen(fnameCut, "w");
 
 	// 파일 처음부터 끝까지 읽어보기, 읽으면서 buffer에 저장시키기
-	while(fgets(buffer, 256, fp)) {
-		line++;
-		if(stristr(buffer, word)) { // buffer에 word가 포함되어 있다면
-			// 대문자로 변경하여 저장
-			
-			
+	while(fgets(buffer, 256, fp)) { // 문자 가져오기
+		corr = strcasestr(buffer, word);
+
+		while(corr != NULL) {
+			corrTemp = (char *)malloc(sizeof(char) * 1024); // corrTemp 메모리 할당
+			strcpy(corrTemp, corr); // corrTemp에 내용 복사  
+			strupr(corrTemp); // 대문자로 변환
+			strncpy(corr, corrTemp, strlen(word)); // 글자 수만 corr 로 다시 복사  
+			corr ++; // corr에 더해줌 
+			corr = strcasestr(corr, word); // 이 줄을 while 문 마지막에 넣어야 오류가 안 남  
 		}
+		fputs(buffer, fp_cap);
 	}
-	
+	fclose(fp_cap);
 	fclose(fp);
 	return 0;
 }
